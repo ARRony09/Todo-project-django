886b806ec4aaa9769605574313b97f4289c7e052
ARRony09 <marrony92@gmail.com>
2021-07-23 16:20:14 +0000

    Remove o ld stuff

--- todowo/settings.py
+++ todowo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0etua$$rq*lo=m#ikbk#2#^(@5s26p_k(o0t$48+dhf)m+m&kd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 LOGIN_URL = '/login'
 
